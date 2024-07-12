2fb640e05c50d2ff04ac31341a11a3c2f462db79
ly <564066938@qq.com>
2024-07-12 15:16:20 +0800

    l

---
+++ src/main/java/com/liyang/runtime_dynamic_datasource/RuntimeDynamicDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.liyang.runtime_dynamic_datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntimeDynamicDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuntimeDynamicDatasourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liyang/runtime_dynamic_datasource/RuntimeDynamicDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyang.runtime_dynamic_datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntimeDynamicDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
